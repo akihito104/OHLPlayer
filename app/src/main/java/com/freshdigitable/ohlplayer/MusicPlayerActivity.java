fc5ec71c8f056c0f9d384f002809ab53c3718de7
Matsuda, Akihit <matsuda104@gmail.com>
2017-05-29 01:09:07 +0900

    refactored MusicPlayerActivity

--- app/src/main/java/com/freshdigitable/ohlplayer/MusicPlayerActivity.java
+++ app/src/main/java/com/freshdigitable/ohlplayer/MusicPlayerActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.WindowManager;
@@ -27,70 +29,53 @@ import com.google.android.exoplayer2.util.Util;
 import java.io.IOException;
 
 import static com.google.android.exoplayer2.ExoPlayerFactory.DEFAULT_ALLOWED_VIDEO_JOINING_TIME_MS;
+import static com.google.android.exoplayer2.SimpleExoPlayer.EXTENSION_RENDERER_MODE_OFF;
 
 public class MusicPlayerActivity extends AppCompatActivity {
   private static final String TAG = MusicPlayerActivity.class.getSimpleName();
   private SimpleExoPlayer simpleExoPlayer;
   private PlaybackControlView controller;
   private Switch ohlToggle;
-  private DebugTextViewHelper debugTextViewHelper;
   private final PlayItemStore playItemStore = new PlayItemStore();
-
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_music_player);
+    controller = (PlaybackControlView) findViewById(R.id.player_controller);
+    ohlToggle = (Switch) findViewById(R.id.ohl_toggle);
     getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-    final ConvoTask convoTask;
-    try {
-      convoTask = StereoHRTFConvoTask.create(getApplicationContext());
-    } catch (IOException e) {
-      Log.e(TAG, "onCreate: ", e);
-      return;
-    }
-    final OHLAudioProcessor ohlAudioProcessor = new OHLAudioProcessor(convoTask);
+    final OHLAudioProcessor ohlAudioProcessor = createProcessor(getApplicationContext());
+    if (ohlAudioProcessor == null) return;
 
-    TrackSelector trackSelector = new DefaultTrackSelector();
-    final DefaultLoadControl loadControl = new DefaultLoadControl();
-    simpleExoPlayer = new SimpleExoPlayer(getApplicationContext(), trackSelector, loadControl,
-        null, SimpleExoPlayer.EXTENSION_RENDERER_MODE_OFF,DEFAULT_ALLOWED_VIDEO_JOINING_TIME_MS) {
-      @Override
-      protected AudioProcessor[] buildAudioProcessors() {
-        return new AudioProcessor[]{ohlAudioProcessor};
-      }
-    };
-    controller = (PlaybackControlView) findViewById(R.id.player_controller);
+    simpleExoPlayer = createPlayer(getApplicationContext(), ohlAudioProcessor);
     controller.setPlayer(simpleExoPlayer);
     controller.show();
-    ohlToggle = (Switch) findViewById(R.id.ohl_toggle);
     ohlToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
       @Override
       public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         ohlAudioProcessor.setEnabled(isChecked);
       }
     });
-    final TextView debugText = (TextView) findViewById(R.id.player_debug);
-    debugTextViewHelper = new DebugTextViewHelper(simpleExoPlayer, debugText);
 
-    final DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-    final DefaultDataSourceFactory dataSourceFactory
-        = new DefaultDataSourceFactory(getApplicationContext(), Util.getUserAgent(getApplicationContext(), "ohlplayer"), bandwidthMeter);
-    final DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-
-    Uri uri = getIntent().getData();
-    final ExtractorMediaSource extractorMediaSource = new ExtractorMediaSource(uri, dataSourceFactory, extractorsFactory, null, null);
+    final ExtractorMediaSource extractorMediaSource
+        = createExtractorMediaSource(getApplicationContext(), getUri());
     simpleExoPlayer.prepare(extractorMediaSource);
   }
+
+  private DebugTextViewHelper debugTextViewHelper;
 
   @Override
   protected void onStart() {
     super.onStart();
     playItemStore.open();
-    MusicItem item = playItemStore.findByPath(getIntent().getStringExtra("path"));
+    final MusicItem item = playItemStore.findByPath(getPath());
     ((TextView) findViewById(R.id.player_title)).setText(item.getTitle());
     ((TextView) findViewById(R.id.player_artist)).setText(item.getArtist());
+
+    final TextView debugText = (TextView) findViewById(R.id.player_debug);
+    debugTextViewHelper = new DebugTextViewHelper(simpleExoPlayer, debugText);
     debugTextViewHelper.start();
   }
 
@@ -108,23 +93,68 @@ public class MusicPlayerActivity extends AppCompatActivity {
       simpleExoPlayer.stop();
       simpleExoPlayer.release();
     }
-    if (controller != null) {
-      controller.setPlayer(null);
-    }
-    if (ohlToggle != null) {
-      ohlToggle.setOnCheckedChangeListener(null);
-    }
+    controller.setPlayer(null);
+    ohlToggle.setOnCheckedChangeListener(null);
   }
 
-  static Intent createIntent(Context context, MusicItem item) {
-    Intent intent = new Intent(context, MusicPlayerActivity.class);
+  private static final String EXTRA_PATH = "path";
+
+  public static Intent createIntent(@NonNull Context context, @NonNull MusicItem item) {
+    final Intent intent = new Intent(context, MusicPlayerActivity.class);
     intent.setData(item.getUri());
-    intent.putExtra("path", item.getPath());
+    intent.putExtra(EXTRA_PATH, item.getPath());
     return intent;
   }
 
-  static void start(Context context, MusicItem item) {
-    Intent intent = createIntent(context, item);
+  private Uri getUri() {
+    return getIntent().getData();
+  }
+
+  private String getPath() {
+    return getIntent().getStringExtra(EXTRA_PATH);
+  }
+
+  public static void start(@NonNull Context context, @NonNull MusicItem item) {
+    final Intent intent = createIntent(context, item);
     context.startActivity(intent);
+  }
+
+  @Nullable
+  private static OHLAudioProcessor createProcessor(@NonNull Context context) {
+    final ConvoTask convoTask;
+    try {
+      convoTask = StereoHRTFConvoTask.create(context);
+    } catch (IOException e) {
+      Log.e(TAG, "createProcessor: ", e);
+      return null;
+    }
+    return new OHLAudioProcessor(convoTask);
+  }
+
+  @NonNull
+  private static SimpleExoPlayer createPlayer(@NonNull Context context,
+                                              @NonNull final OHLAudioProcessor ohlAudioProcessor) {
+    final TrackSelector trackSelector = new DefaultTrackSelector();
+    final DefaultLoadControl loadControl = new DefaultLoadControl();
+    return new SimpleExoPlayer(context, trackSelector, loadControl, null,
+        EXTENSION_RENDERER_MODE_OFF, DEFAULT_ALLOWED_VIDEO_JOINING_TIME_MS) {
+      @Override
+      protected AudioProcessor[] buildAudioProcessors() {
+        return new AudioProcessor[]{ohlAudioProcessor};
+      }
+    };
+  }
+
+  private static final String USER_AGENT_NAME = "ohlplayer";
+
+  @NonNull
+  private static ExtractorMediaSource createExtractorMediaSource(@NonNull Context context,
+                                                                 @NonNull Uri dataSource) {
+    final DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+    final String userAgent = Util.getUserAgent(context, USER_AGENT_NAME);
+    final DefaultDataSourceFactory dataSourceFactory
+        = new DefaultDataSourceFactory(context, userAgent, bandwidthMeter);
+    final DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+    return new ExtractorMediaSource(dataSource, dataSourceFactory, extractorsFactory, null, null);
   }
 }
