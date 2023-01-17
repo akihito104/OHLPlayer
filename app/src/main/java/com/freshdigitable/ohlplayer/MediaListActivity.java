c9b68ee7f3a0285ef6b4c75dbbe4a938d0cc4b8b
akihito104 <matsuda104@gmail.com>
2023-01-17 17:01:16 +0900

    updated api 33

--- app/src/main/java/com/freshdigitable/ohlplayer/MediaListActivity.java
+++ app/src/main/java/com/freshdigitable/ohlplayer/MediaListActivity.java
@@ -1,17 +1,22 @@
 package com.freshdigitable.ohlplayer;
 
-import android.Manifest;
+import android.content.ContentUris;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +26,7 @@ import com.freshdigitable.ohlplayer.store.PlayableItem;
 import com.freshdigitable.ohlplayer.store.PlayableItemStore;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -36,7 +42,7 @@ public class MediaListActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_media_list);
 
-    listView = (RecyclerView) findViewById(R.id.list);
+    listView = findViewById(R.id.list);
     final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
     linearLayoutManager.setAutoMeasureEnabled(true);
     listView.setLayoutManager(linearLayoutManager);
@@ -48,18 +54,7 @@ public class MediaListActivity extends AppCompatActivity {
     super.onStart();
     playableItemStore.open();
     final ViewAdapter adapter = new ViewAdapter(playableItemStore);
-    if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-        == PackageManager.PERMISSION_GRANTED) {
-      addLocalMediaFiles();
-    } else {
-      if (!ActivityCompat.shouldShowRequestPermissionRationale(
-          this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-        ActivityCompat.requestPermissions(
-            this,
-            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-            100);
-      }
-    }
+    addLocalMediaFiles();
     listView.setAdapter(adapter);
   }
 
@@ -78,34 +73,31 @@ public class MediaListActivity extends AppCompatActivity {
   private void addLocalMediaFiles() {
     final List<PlayableItem> playableItems = new ArrayList<>();
     for (String type : Arrays.asList(Environment.DIRECTORY_MUSIC, Environment.DIRECTORY_MOVIES)) {
-      final List<PlayableItem> items = findNewMediaFiles(type);
+      final List<PlayableItem> items = findPlayableItems(type);
       playableItems.addAll(items);
     }
     playableItemStore.registerIfAbsent(playableItems);
   }
 
-  private List<PlayableItem> findNewMediaFiles(String type) {
-    final File externalFilesDir = Environment.getExternalStoragePublicDirectory(type);
-    final String[] fileList = externalFilesDir.list();
-    final ArrayList<String> files = new ArrayList<>();
-    for (String name : fileList) {
-      final File f = new File(externalFilesDir, name);
-      final String path = f.getAbsolutePath();
-      if (playableItemStore.findByPath(path) == null) {
-        files.add(path);
-      }
-    }
+  private List<PlayableItem> findPlayableItems(String type) {
+    final List<Uri> files = findMediaFiles(type);
     if (files.isEmpty()) {
       return Collections.emptyList();
     }
 
     final List<PlayableItem> items = new ArrayList<>();
-    for (String path : files) {
+    for (final Uri uri : files) {
       final MediaMetadataRetriever metadataRetriever = new MediaMetadataRetriever();
-      metadataRetriever.setDataSource(path);
+      metadataRetriever.setDataSource(getApplicationContext(), uri);
       final String title = metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
       final String artist = metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
-      metadataRetriever.release();
+      try {
+        metadataRetriever.release();
+      } catch (IOException e) {
+        e.printStackTrace();
+        throw new RuntimeException(e);
+      }
+      final String path = uri.toString();
       final PlayableItem item = new PlayableItem.Builder(path)
           .title(TextUtils.isEmpty(title) ? new File(path).getName() : title)
           .artist(artist)
@@ -113,6 +105,88 @@ public class MediaListActivity extends AppCompatActivity {
       items.add(item);
     }
     return items;
+  }
+
+  @NonNull
+  private List<Uri> findMediaFiles(String type) {
+    Log.d("MediaListActivity", "findPlayableFiles: " + type);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      return getFileListQ(type);
+    } else {
+      return getFileList(type);
+    }
+  }
+
+  @NonNull
+  private List<Uri> getFileList(String type) {
+    final File externalFilesDir = Environment.getExternalStoragePublicDirectory(type);
+    final String[] fileList = externalFilesDir.list();
+    if (fileList == null) {
+      return Collections.emptyList();
+    }
+    final List<Uri> files = new ArrayList<>();
+    for (String name : fileList) {
+      Log.d("MediaListActivity", "findPlayableFiles> file: " + name);
+      final File f = new File(externalFilesDir, name);
+      if (f.isDirectory()) {
+        continue;
+      }
+      final String path = f.getAbsolutePath();
+      if (playableItemStore.findByPath(path) == null) {
+        files.add(Uri.fromFile(f));
+      }
+    }
+    return files;
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.Q)
+  private List<Uri> getFileListQ(String type) {
+    final Uri collection;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      if (Environment.DIRECTORY_MUSIC.equals(type)) {
+        collection = MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL);
+      } else if (Environment.DIRECTORY_MOVIES.equals(type)) {
+        collection = MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL);
+      } else {
+        throw new IllegalArgumentException();
+      }
+    } else {
+      if (Environment.DIRECTORY_MUSIC.equals(type)) {
+        collection = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+      } else if (Environment.DIRECTORY_MOVIES.equals(type)) {
+        collection = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+      } else {
+        throw new IllegalArgumentException();
+      }
+    }
+
+    final String[] projection = new String[] {
+            MediaStore.Video.Media._ID,
+            MediaStore.MediaColumns.RELATIVE_PATH,
+    };
+    try (final Cursor cursor = getApplicationContext().getContentResolver().query(
+            collection,
+            projection,
+            null,
+            null,
+            null
+    )) {
+      // Cache column indices.
+      final int idColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID);
+      final int relativePathColumn = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.RELATIVE_PATH);
+      final List<Uri> files = new ArrayList<>();
+      while (cursor.moveToNext()) {
+        final long id = cursor.getLong(idColumn);
+        final String relativePath = cursor.getString(relativePathColumn);
+        if (relativePath.startsWith("Music/") || relativePath.startsWith("Video/")) {
+          final Uri contentUri = ContentUris.withAppendedId(collection, id);
+          Log.d("MediaListActivity", "getFileListQ: " + contentUri.toString() +
+                  ", " + relativePath);
+          files.add(contentUri);
+        }
+      }
+      return files;
+    }
   }
 
   @Override
@@ -127,20 +201,18 @@ public class MediaListActivity extends AppCompatActivity {
 
     ViewAdapter(PlayableItemStore store) {
       this.playableItemStore = store;
-      this.playableItemStore.addEventListener(new PlayableItemStore.StoreEventListener() {
-        @Override
-        public void onStoreUpdate(PlayableItemStore.EventType type, int range, int length) {
-          if (type == PlayableItemStore.EventType.INSERT) {
-            notifyItemRangeInserted(range, length);
-          } else if (type == PlayableItemStore.EventType.CHANGE) {
-            notifyItemRangeChanged(range, length);
-          } else if (type == PlayableItemStore.EventType.DELETE) {
-            notifyItemRangeRemoved(range, length);
-          }
+      this.playableItemStore.addEventListener((type, range, length) -> {
+        if (type == PlayableItemStore.EventType.INSERT) {
+          notifyItemRangeInserted(range, length);
+        } else if (type == PlayableItemStore.EventType.CHANGE) {
+          notifyItemRangeChanged(range, length);
+        } else if (type == PlayableItemStore.EventType.DELETE) {
+          notifyItemRangeRemoved(range, length);
         }
       });
     }
 
+    @NonNull
     @Override
     public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
       final View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_media_list_item, parent, false);
@@ -152,16 +224,11 @@ public class MediaListActivity extends AppCompatActivity {
       final PlayableItem item = playableItemStore.get(position);
       holder.title.setText(item.getTitle());
       holder.artist.setText(item.getArtist());
-      holder.itemView.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          MediaPlayerActivity.start(v.getContext(), item);
-        }
-      });
+      holder.itemView.setOnClickListener(v -> MediaPlayerActivity.start(v.getContext(), item));
     }
 
     @Override
-    public void onViewRecycled(Holder holder) {
+    public void onViewRecycled(@NonNull Holder holder) {
       super.onViewRecycled(holder);
       holder.itemView.setOnClickListener(null);
     }
@@ -177,8 +244,8 @@ public class MediaListActivity extends AppCompatActivity {
 
       Holder(View itemView) {
         super(itemView);
-        title = (TextView) itemView.findViewById(R.id.list_title);
-        artist = (TextView) itemView.findViewById(R.id.list_artist);
+        title = itemView.findViewById(R.id.list_title);
+        artist = itemView.findViewById(R.id.list_artist);
       }
     }
   }
